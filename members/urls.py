7c9ebcd0ca932632f6b6bc0602b20bea5d2510e9
Vivek2371984 <aithagonivivekvardhan@gmail.com>
2025-03-20 10:42:46 +0000

    added admin login page and the dashboard

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 from .views import register, home, user_login  # ✅ Import user_login
+from .views import admin_login, admin_dashboard, admin_logout
+from .views import registered_members
+from .views import pending_members, approve_member, reject_member
+
+
 
 urlpatterns = [
     path("", home, name="home"),  # Home page
     path("register/", register, name="register"),
     path("login/", user_login, name="login"),  # Login page
+    path("admin-login/", admin_login, name="admin_login"),
+    path("admin-dashboard/", admin_dashboard, name="admin_dashboard"),
+    path("admin-logout/", admin_logout, name="admin_logout"),
+    path("registered-members/", registered_members, name="registered_members"),
+     path("pending-members/", pending_members, name="pending_members"),
+    path("approve-member/<int:member_id>/", approve_member, name="approve_member"),
+    path("reject-member/<int:member_id>/", reject_member, name="reject_member"),
+
 ]
