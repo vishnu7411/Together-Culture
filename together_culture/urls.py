2b57abb59de96ddc8d85b707c99be9c5277e5825
Keerthana427-hub <Keerthanashivakumar427@gmail.com>
2025-03-29 14:42:02 +0000

    Delete together_culture directory

--- together_culture/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # Import 'include'
-from events import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('members.urls')),  # Include the URLs from 'members' app
-    path('review/', views.review_page, name='review')
-]
